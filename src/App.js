2d8689e8c76043eaa2ab408604df5009e34a2555
Varala Nandu Swapnik <99593195+swapnik-iitkgp@users.noreply.github.com>
2024-07-15 14:26:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/my-portfolio" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
